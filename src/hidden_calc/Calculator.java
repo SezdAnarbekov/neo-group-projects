50be9e7761b2afc871fbd2ca82123e5c9864f730
Cyezd Anarbekov <jumushuchun@gmail.com>
2017-10-10 14:38:37 -0500

    Deleted .idea/vcs.xml

--- src/hidden_calc/Calculator.java
+++ src/hidden_calc/Calculator.java
@@ -4,50 +4,49 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import static java.awt.Color.CYAN;
-import static java.awt.Color.HSBtoRGB;
 
-public class Calculator implements ActionListener{
- String[] characters ={"1","2","3","C","4","5","6","+","7","8","9","-","*","0","/","="};
- JButton[] buttons = new JButton[16];
- JFrame frame;
- JPasswordField text;
-String operator = "=";
-long result = 0;
-double result2 = 0;
+public class Calculator implements ActionListener {
+    String[] characters = {"1", "2", "3", "C", "4", "5", "6", "+", "7", "8", "9", "-", "*", "0", "/", "="};
+    JButton[] buttons = new JButton[16];
+    JFrame frame;
+    JPasswordField text;
+    String operator = "=";
+    long result = 0;
+    double result2 = 0;
     boolean calculating = true;
- public Calculator(){
 
-     frame = new JFrame();
-     frame.setLayout(null);
-     frame.setVisible(true);
-     frame.setSize(200,275);
-     frame.setTitle("Syezd's Calculator");
-    text = new JPasswordField("0", 20);
-   text.setBackground(new Color(0,145,255));
-    text.setEditable(false);
-text.setHorizontalAlignment(SwingConstants.RIGHT);
-text.setEchoChar((char)0);
-    frame.add(text);
-    text.setBounds(0,0,200,50);
-     int n = 0;
-     for (int i = 0; i < 16; i++){
-         buttons[i] = new JButton(characters[i]);
-         if (i%4==0)
-             n=n+50;
-         buttons[i].setBounds((i%4)*50, 0 + n, 50, 50);
-         buttons[i].addActionListener(this);
-        buttons[i].setBackground(new Color(140,146,151));
-       buttons[i].setOpaque(true);
-         frame.add(buttons[i]);
+    public Calculator() {
 
-     }
- }
+        frame = new JFrame();
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setSize(200, 275);
+        frame.setTitle("Syezd's Calculator");
+        text = new JPasswordField("0", 20);
+        text.setBackground(new Color(0, 145, 255));
+        text.setEditable(false);
+        text.setHorizontalAlignment(SwingConstants.RIGHT);
+        text.setEchoChar((char) 0);
+        frame.add(text);
+        text.setBounds(0, 0, 200, 50);
+        int n = 0;
+        for (int i = 0; i < 16; i++) {
+            buttons[i] = new JButton(characters[i]);
+            if (i % 4 == 0)
+                n = n + 50;
+            buttons[i].setBounds((i % 4) * 50, 0 + n, 50, 50);
+            buttons[i].addActionListener(this);
+            buttons[i].setBackground(new Color(140, 146, 151));
+            buttons[i].setOpaque(true);
+            frame.add(buttons[i]);
+
+        }
+    }
 
     public void actionPerformed(ActionEvent e) {
 
         String cmd = e.getActionCommand();
-        if ('0' <= cmd.charAt(0) && cmd.charAt(0) <= '9' ) {
+        if ('0' <= cmd.charAt(0) && cmd.charAt(0) <= '9') {
             if (calculating)
                 text.setText(cmd);
             else
@@ -68,10 +67,10 @@ text.setEchoChar((char)0);
             }
 
         }
-        if("C" == e.getActionCommand())
+        if ("C" == e.getActionCommand())
             text.setText("0");
-       if (text.getText().equals("10") && (buttons[15].equals(e.getActionCommand())))
-         JOptionPane.showMessageDialog(frame, "kkkkkk");
+        if (text.getText().equals("10") && (buttons[15].equals(e.getActionCommand())))
+            JOptionPane.showMessageDialog(frame, "kkkkkk");
     }
 
     public void calculate(long n) {
@@ -86,10 +85,11 @@ text.setEchoChar((char)0);
         else if (operator.equals("="))
             result = n;
 
-          text.setText("" + result);
+        text.setText("" + result);
 
 
     }
+
     public static void main(String[] args) {
         new Calculator();
     }
